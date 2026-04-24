7b97b54bedc174c232fd1705891fab08a22d0b76
Richard <richard.oliveira.senai@gmail.com>
2026-04-24 12:38:15 -0300

    EntregaDeTestes

---
+++ src/test/java/com/deliverytech/delivery_api/controller/ClienteControllerTest.java
@@ -0,0 +1,176 @@
+// package com.deliverytech.delivery_api.controller;
+
+// import java.util.List;
+
+// import static org.mockito.Answers.*;
+// import static org.mockito.ArgumentMatchers.*;
+// import static org.mockito.Mockito.*;
+
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Import;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.http.MediaType;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.context.annotation.FilterType;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.PageImpl;
+// import org.springframework.data.domain.Pageable;
+
+// import com.deliverytech.delivery_api.config.SecurityTestConfig;
+// import com.deliverytech.delivery_api.dto.requests.ClienteDTO;
+// import com.deliverytech.delivery_api.dto.responses.ClienteResponseDTO;
+// import com.deliverytech.delivery_api.exception.BusinessException;
+// import com.deliverytech.delivery_api.exception.EntityNotFoundException;
+// import com.deliverytech.delivery_api.security.JwtAuthenticationFilter;
+// import com.deliverytech.delivery_api.service.ClienteService;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+// import org.springframework.security.test.context.support.WithMockUser;
+
+
+// @WebMvcTest(
+//     controllers = ClienteController.class,
+//     excludeFilters = @ComponentScan.Filter(
+//         type = FilterType.ASSIGNABLE_TYPE,
+//         classes = JwtAuthenticationFilter.class
+//     )
+// )
+// @Import(SecurityTestConfig.class)
+// public class ClienteControllerTest {
+//     @Autowired
+//     private MockMvc mockMvc;
+
+//     @Autowired
+//     private ObjectMapper objectMapper;
+
+//     @MockBean
+//     private ClienteService service;
+
+
+//     private ClienteDTO clienteValido(){
+//         ClienteDTO dto = new ClienteDTO();
+//         dto.setNome("João Silva");
+//         dto.setTelefone("(11)9999-9999");
+//         dto.setEndereco("Rua A, 123");
+//         return dto;
+//     }
+
+//     @Test
+//     @WithMockUser(username = "teste@gmail.com")
+//     @DisplayName("Deve cadastrar o cliente e retornar 201 quando cadastrado.")
+//     void deveCadastrarCliente() throws Exception{
+//         ClienteDTO dto =  clienteValido();
+
+//         ClienteResponseDTO response = new ClienteResponseDTO();
+//         response.setNome("João Silva");
+
+//         when(service.cadastrar(any(ClienteDTO.class), eq("teste@gmail.com")))
+//             .thenReturn(response);
+
+//         mockMvc.perform(post("/api/clientes/cadastrar")
+//             .contentType(MediaType.APPLICATION_JSON)
+//             .content(objectMapper.writeValueAsString(dto)))
+//             .andDo(print())
+//             .andExpect(status().isCreated())
+//             .andExpect(jsonPath("$.nome").value("João Silva"));
+//     }
+
+//     @Test
+//     @DisplayName("Deve retornar 400 quando o DTO for inválido.")
+//     void deveRetornar400QuandoDTOInvalido() throws Exception{
+//         ClienteDTO dto = new ClienteDTO();
+//         mockMvc.perform(post("/api/clientes/cadastrar")
+//             .contentType(MediaType.APPLICATION_JSON)
+//             .content(objectMapper.writeValueAsString(dto)))
+//             .andExpect(status().isBadRequest());
+//     }
+
+//     @Test
+//     @WithMockUser(username = "teste@gmail.com")
+//     void deveRetornarErroAoCadastrarClienteDuplicado() throws Exception {
+//         ClienteDTO dto =  clienteValido();
+
+//         when(service.cadastrar(any(ClienteDTO.class), eq("teste@gmail.com")))
+//         .thenThrow(new BusinessException("Cliente já cadastrado para este usuário."));
+
+//         mockMvc.perform(post("/api/clientes/cadastrar")
+//             .contentType(MediaType.APPLICATION_JSON)
+//             .content(objectMapper.writeValueAsString(dto)))
+//             .andExpect(status().isConflict());
+
+//     }
+
+
+//     @Test
+//     @WithMockUser(username = "teste@gmail.com")
+//     void deveListarClientesAtivos() throws Exception{
+//         ClienteResponseDTO cliente = new ClienteResponseDTO();
+//         cliente.setNome("Elaine Soares");
+
+//         List<ClienteResponseDTO> lista = List.of(cliente);
+
+//         Page<ClienteResponseDTO> pageResponse = new PageImpl<>(lista);
+
+//         when(service.listarAtivos(any(Pageable.class))).thenReturn(pageResponse);
+
+//         mockMvc.perform(get("/api/clientes")
+//             .param("page", "0")
+//             .param("size", "10")
+//             .contentType(MediaType.APPLICATION_JSON))
+//             .andDo(print())
+//             .andExpect(status().isOk())
+//             .andExpect(jsonPath("$.content").isArray())
+//             .andExpect(jsonPath("$.content[0].nome").value("Elaine Soares"));
+
+//     }
+
+//     @Test
+//     @DisplayName("Deve buscar cliente por ID.")
+//     void deveBuscarPorId() throws Exception{
+//         ClienteResponseDTO response = new ClienteResponseDTO();
+//         response.setNome("Pedro Chaves");
+
+//         when(service.buscarPorId(1L)).thenReturn(response);
+
+//         mockMvc.perform(get("/api/clientes/{id}", 1L))
+//         .andExpect(status().isOk())
+//         .andExpect(jsonPath("$.data.nome").value("Pedro Chaves"));
+//     }
+
+//     @Test
+//     void deveRetornar404AoBuscarIdInexistente()throws Exception{
+//         Long idInexistente = 999L;
+
+//         when(service.buscarPorId(idInexistente))
+//         .thenThrow(new EntityNotFoundException("Cliente não encontrado."));
+
+//         mockMvc.perform(get("/api/clientes/{id}", idInexistente)
+//             .contentType(MediaType.APPLICATION_JSON))
+//             .andDo(print())
+//             .andExpect(status().isNotFound());
+//     }
+
+
+//     @Test
+//     void deveInativarCliente() throws Exception{
+//         ClienteResponseDTO response = new ClienteResponseDTO();
+
+//         response.setId(1L);
+
+//         when(service.inativar(1L)).thenReturn(response);
+
+//         mockMvc.perform(put("/api/clientes/{id}/inativar-cliente", 1L))
+//         .andExpect(status().isOk())
+//         .andExpect(jsonPath("$.id").value(1L));
+//     }
+
+// }
---
+++ src/test/java/com/deliverytech/delivery_api/service/ClienteServiceIT.java
@@ -0,0 +1,159 @@
+// package com.deliverytech.delivery_api.service;
+
+// import java.util.List;
+// import java.util.Optional;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+// import static org.junit.jupiter.api.Assertions.assertThrows;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.ArgumentMatchers.eq;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import static org.mockito.Mockito.atLeastOnce;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
+// import org.mockito.junit.jupiter.MockitoExtension;
+// import org.modelmapper.ModelMapper;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.PageImpl;
+// import org.springframework.data.domain.PageRequest;
+// import org.springframework.data.domain.Pageable;
+
+// import com.deliverytech.delivery_api.dto.requests.ClienteDTO;
+// import com.deliverytech.delivery_api.dto.responses.ClienteResponseDTO;
+// import com.deliverytech.delivery_api.enums.Role;
+// import com.deliverytech.delivery_api.exception.BusinessException;
+// import com.deliverytech.delivery_api.exception.EntityNotFoundException;
+// import com.deliverytech.delivery_api.model.Cliente;
+// import com.deliverytech.delivery_api.model.Usuario;
+// import com.deliverytech.delivery_api.repository.ClienteRepository;
+// import com.deliverytech.delivery_api.repository.UsuarioRepository;
+
+// @ExtendWith(MockitoExtension.class)
+// public class ClienteServiceIT {
+
+//     @Mock
+//     private ClienteRepository clienteRepository;
+
+//     @Mock
+//     private UsuarioRepository usuarioRepository;
+
+//     @Mock
+//     private ModelMapper mapper;
+
+//     @InjectMocks
+//     private ClienteService service;
+
+//     private Usuario usuario;
+//     private Cliente cliente; 
+//     private ClienteDTO dto;
+//     private ClienteResponseDTO clienteResponseDTO;
+
+//     @BeforeEach
+//     void setup(){
+//         dto = new ClienteDTO();
+
+//         usuario = new Usuario();
+//         usuario.setId(1L);
+//         usuario.setEmail("lucas@gmail.com");
+//         usuario.setRole(Role.CLIENTE);
+//         usuario.setAtivo(true);
+
+//         cliente = new Cliente();
+//         cliente.setId(1L);
+//         cliente.setUsuario(usuario);
+//         cliente.setEmail(usuario.getEmail());
+//         cliente.setAtivo(true);
+
+//         clienteResponseDTO = new ClienteResponseDTO();
+//         clienteResponseDTO.setId(1L);
+//         clienteResponseDTO.setEmail("lucas@gmail.com");
+
+//     }
+
+//     @Test
+//     void deveCadastrarClienteComSucesso(){
+//         when(usuarioRepository.findByEmail("lucas@gmail.com")).thenReturn(Optional.of(usuario));
+//         when(clienteRepository.existsByUsuario_Id(1L)).thenReturn(false);
+//         when(mapper.map(dto, Cliente.class)).thenReturn(cliente);
+//         when(clienteRepository.save(any(Cliente.class))).thenReturn(cliente);
+//         when(mapper.map(cliente, ClienteResponseDTO.class)).thenReturn(clienteResponseDTO);
+
+//         ClienteResponseDTO resultado = service.cadastrar(dto, "lucas@gmail.com");
+
+//         assertNotNull(resultado);
+//         assertEquals("lucas@gmail.com", resultado.getEmail());
+//         verify(clienteRepository).save(any());
+//     }
+
+//     @Test
+//     void deveLancarExcecaoQuandoClienteJaExistir(){
+//         when(usuarioRepository.findByEmail("lucas@gmail.com")).thenReturn(Optional.of(usuario));
+//         when(clienteRepository.existsByUsuario_Id(1L)).thenReturn(true);
+
+//         BusinessException ex = assertThrows(BusinessException.class, ()-> {
+//         service.cadastrar(dto, "lucas@gmail.com");
+//         });
+
+//         assertEquals("Cliente já cadastrado para este usuário.", ex.getMessage());
+//     }
+
+//     @Test
+//     void deveListarClientesAtivos() {
+//         Pageable pageable = PageRequest.of(0, 10);
+//         Page<Cliente> page = new PageImpl<>(List.of(cliente));
+
+        
+//         when(clienteRepository.findByAtivoTrue(pageable)).thenReturn(page);
+//         when(mapper.map(any(Cliente.class), eq(ClienteResponseDTO.class))).thenReturn(clienteResponseDTO);
+
+//         Page<ClienteResponseDTO> resultado = service.listarAtivos(pageable);
+
+//         assertNotNull(resultado);
+//         assertEquals(1, resultado.getTotalElements());
+//         verify(mapper, atLeastOnce()).map(any(Cliente.class), eq(ClienteResponseDTO.class));
+//     }
+
+//     @Test
+//     void deveBuscarClientePorIdComSucesso(){
+//         when(clienteRepository.findById(1L)).thenReturn(Optional.of(cliente));
+//         when(mapper.map(cliente, ClienteResponseDTO.class)).thenReturn(clienteResponseDTO);
+
+//         ClienteResponseDTO resultado = service.buscarPorId(1L);
+//         assertNotNull(resultado);
+//     }
+
+//     @Test
+//     void deveLancarExcecaoQuandoClienteNaoEncontrado(){
+//         when(clienteRepository.findById(1L)).thenReturn(Optional.empty());
+
+//         assertThrows(EntityNotFoundException.class, ()->{
+//             service.buscarPorId(1L);
+//         });
+//     }
+
+//     @Test
+//     void deveInativarOuAtivarCliente(){
+//         when(clienteRepository.findById(1L)).thenReturn(Optional.of(cliente));
+//         when(clienteRepository.save(any())).thenReturn(cliente);
+//         when(mapper.map(cliente, ClienteResponseDTO.class)).thenReturn(clienteResponseDTO);
+
+//         ClienteResponseDTO resultado = service.inativar(1L);
+
+//         assertNotNull(resultado);
+//         verify(clienteRepository).save(any());
+//     }
+
+//     @Test
+//     void deveLancarExcecaoQuandoIdNaoEncontradoEmInativar(){
+//         when(clienteRepository.findById(1L)).thenReturn(Optional.empty());
+//         assertThrows(EntityNotFoundException.class, ()->{
+//             service.inativar(1L);
+//         });
+//     }
+
+// }
